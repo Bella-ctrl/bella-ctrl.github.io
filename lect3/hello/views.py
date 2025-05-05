e6068cf4fad217cd575b3781d3843a4d60e24e58
Bella-ctrl <v.30612338.gb@gmail.com>
2025-05-05 19:35:00 +0000

    added new app to django project

--- lect3/hello/views.py
+++ lect3/hello/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request, "hello/index.html")
 
 def brian(request):
     return HttpResponse("Hello, Gabriela!")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
