47a4a0de9b39dda307f2c12dd8ecb2dcc86482bc
Bella-ctrl <v.30612338.gb@gmail.com>
2025-05-06 13:54:20 +0000

    added new app and succesfully linked it to the project

--- lect3/newyear/apps.py
+++ lect3/newyear/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NewyConfig(AppConfig):
+class NewyearConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "newy"
+    name = "newyear"
