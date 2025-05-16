6f27df2223207f4d99ba02b24fc09fa118675562
Bella-ctrl <v.30612338.gb@gmail.com>
2025-05-16 19:23:24 +0000

    registered the models in admin.py

--- airline/commerce/auctions/admin.py
+++ airline/commerce/auctions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Listing, Bids, Comments
+
+admin.site.register(Listing)
+admin.site.register(Bids)
+admin.site.register(Comments)
