b6a25d046a1633b99913ba0d41b488c952c574bf
Bella-ctrl <v.30612338.gb@gmail.com>
2025-05-17 14:13:48 +0000

    added watchlist as path in urls.py

--- airline/commerce/auctions/urls.py
+++ airline/commerce/auctions/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"), 
     path("create", views.create_listing, name="create_listing"),
-    path("listing/<int:listing_id>/", views.listing_detail, name="listing_detail")
+    path("listing/<int:listing_id>/", views.listing_detail, name="listing_detail"),
+    path("watchlist/", views.watchlist, name="watchlist")
 ]
