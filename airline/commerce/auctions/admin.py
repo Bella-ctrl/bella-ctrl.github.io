95ee561475a1ec257af785c9ab3a441f13ac9c1a
Bella-ctrl <v.30612338.gb@gmail.com>
2025-05-17 17:22:19 +0000

    fixed img issue and changed admin.py as to later create the superuser

--- airline/commerce/auctions/admin.py
+++ airline/commerce/auctions/admin.py
@@ -1,9 +1,34 @@
 from django.contrib import admin
+from .models import User, Listing, Bid, Comment, WatchList
 
 # Register your models here.
-from .models import Listing, Bid, Comment, WatchList
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'creator', 'current_bid', 'category', 'is_active')
+    list_filter = ('is_active', 'category', 'created_at')
+    search_fields = ('title', 'description')
+    readonly_fields = ('created_at',)
 
-admin.site.register(Listing)
-admin.site.register(Bid)
-admin.site.register(Comment)
-admin.site.register(WatchList)
+class BidAdmin(admin.ModelAdmin):
+    list_display = ('listing', 'bidder', 'amount', 'bid_time')
+    list_filter = ('bid_time',)
+    search_fields = ('listing__title', 'bidder__username')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('listing', 'author', 'created_at', 'short_text')
+    list_filter = ('created_at',)
+    search_fields = ('text', 'listing__title')
+    
+    def short_text(self, obj):
+        return obj.text[:50] + '...' if len(obj.text) > 50 else obj.text
+    short_text.short_description = 'Comment Preview'
+
+class WatchListAdmin(admin.ModelAdmin):
+    list_display = ('user', 'listing', 'added_at')
+    list_filter = ('added_at',)
+    search_fields = ('user__username', 'listing__title')
+
+admin.site.register(User)
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Bid, BidAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(WatchList, WatchListAdmin)
