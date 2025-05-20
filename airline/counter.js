4ff6a45c5b6332bc59cec49b80812fb695102b28
Bella-ctrl <v.30612338.gb@gmail.com>
2025-05-20 13:51:37 +0000

    changed counter.js

--- airline/counter.js
+++ airline/counter.js
@@ -3,9 +3,9 @@ function count() {
     counter++;
     document.querySelector("h1").innerHTML = counter;
                 
-    if (counter % 10 === 0) {
-         alert(`Count is now ${counter}`);
-    }
+    // if (counter % 10 === 0) {
+         // alert(`Count is now ${counter}`);
+    // }
 }
 
     document.addEventListener("DOMContentLoaded", function() {
