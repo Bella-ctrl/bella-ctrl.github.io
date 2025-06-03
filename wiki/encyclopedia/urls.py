99a4b74ec0ffa203d477d57fb39274b873d19212
bella-ctrl <v.30612338.gb@gmail.com>
2025-06-03 14:17:23 -0400

    changed url for create as it was throwing unexpected behaviour

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("wiki/<str:title>/", views.entry, name="entry"),
     path("search/", views.search, name="search"),
     path("wiki/edit/<str:title>/", views.edit, name="edit"),
-    path("wiki/create/", views.create, name="create")
+    path("create_page", views.create, name="create")
 ]
