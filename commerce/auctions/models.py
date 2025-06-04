4375516054aaa19d509646b0d94729c2c21d537e
bella-ctrl <v.30612338.gb@gmail.com>
2025-06-04 16:22:51 -0400

    Added extra fields to some models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -6,20 +6,24 @@ class User(AbstractUser):
     pass
 
 class Listings(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=64, blank=True, null=True)
     starting_bid = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
     image_url = models.URLField(blank=True)
     category = models.CharField(max_length=64, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
 
 class Bids(models.Model):
+    id = models.AutoField(primary_key=True)
     listing = models.ForeignKey(Listings, on_delete=models.CASCADE, related_name="bids")
     bid_amount = models.DecimalField(max_digits=10, decimal_places=2)
     bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidder")
 
 class Comments(models.Model):
+    id = models.AutoField(primary_key=True)
     listing = models.ForeignKey(Listings, on_delete=models.CASCADE, related_name="comments")
     comment_text = models.TextField()
     commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
