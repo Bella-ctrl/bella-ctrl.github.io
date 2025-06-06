588df55d8fcd813434c7ca24a950df9dafaaf129
bella-ctrl <v.30612338.gb@gmail.com>
2025-06-06 11:27:25 -0400

    added path to category

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('comment/<int:listing_id>/', views.add_comment, name='add_comment'),
     path('watchlist/', views.watchlist, name='watchlist'),
     path('categories/', views.categories, name='categories'),
+    path('category/<str:category_code>/', views.category_listings, name='category_listings'),
 ]
