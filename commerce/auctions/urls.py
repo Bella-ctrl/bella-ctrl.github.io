777f1d33db4076683a2b9160cf6a8cbf9293bf01
bella-ctrl <v.30612338.gb@gmail.com>
2025-06-06 10:14:45 -0400

    changed urls and styles

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("create_listing", views.create_listing, name="create_listing"),
-    path("listing/<int:listing_id>", views.listing, name="listing"),
-    path("watchlist", views.watchlist, name="watchlist"),
-    path("categories", views.categories, name="categories")
+    path('listing/<int:listing_id>/', views.listing, name='listing'),
+    path('watchlist/<int:listing_id>/', views.watchlist_toggle, name='watchlist_toggle'),
+    path('bid/<int:listing_id>/', views.place_bid, name='place_bid'),
+    path('close/<int:listing_id>/', views.close_auction, name='close_auction'),
+    path('comment/<int:listing_id>/', views.add_comment, name='add_comment'),
 ]
