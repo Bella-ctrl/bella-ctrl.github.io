97e04953ab29223460137a222b1c2ac5448d7421
bella-ctrl <v.30612338.gb@gmail.com>
2025-07-07 15:45:27 -0400

    TypeError

--- print.py
+++ print.py
@@ -4,7 +4,7 @@
 
 n = int(input("Number: "))
 # This throws a TypeError, as if you try to compare a string with an integer.
-### TypeError: the types of values involved 
+### TypeError: the types of values involved are not compatible
 # To fix, turn the input into an integer. 
 if n > 0:
     print("Positive")
