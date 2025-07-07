d30930ba2601441adbf93f024a35ab48d82a7f8d
bella-ctrl <v.30612338.gb@gmail.com>
2025-07-07 15:46:44 -0400

    conditions

--- print.py
+++ print.py
@@ -3,8 +3,11 @@
 #print(f"Hello, {name}!")
 
 n = int(input("Number: "))
-# This throws a TypeError, as if you try to compare a string with an integer.
-### TypeError: the types of values involved are not compatible
-# To fix, turn the input into an integer. 
+# This throws a TypeError - To fix, turn the input into an integer.
+### TypeError: the types of values involved are not compatible 
 if n > 0:
     print("Positive")
+elif n < 0:
+    print("Negative")
+else:
+    print("Zero")
