db5b7c2bb4c6b42f624ae4355ca7700af0280de5
bella-ctrl <v.30612338.gb@gmail.com>
2025-07-07 15:45:01 -0400

    TypeError

--- print.py
+++ print.py
@@ -1,3 +1,10 @@
-name = input("Name: ")
-print("Hello, " + name + "!")
-print(f"Hello, {name}!")
+#name = input("Name: ")
+#print("Hello, " + name + "!")
+#print(f"Hello, {name}!")
+
+n = int(input("Number: "))
+# This throws a TypeError, as if you try to compare a string with an integer.
+### TypeError: the types of values involved 
+# To fix, turn the input into an integer. 
+if n > 0:
+    print("Positive")
