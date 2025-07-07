88b99f4e1a8e98e8eb32632cee214474a1634f77
bella-ctrl <v.30612338.gb@gmail.com>
2025-07-07 15:50:47 -0400

    sequences

--- print.py
+++ print.py
@@ -2,12 +2,24 @@
 #print("Hello, " + name + "!")
 #print(f"Hello, {name}!")
 
-n = int(input("Number: "))
+###########n = int(input("Number: "))
 # This throws a TypeError - To fix, turn the input into an integer.
 ### TypeError: the types of values involved are not compatible 
-if n > 0:
-    print("Positive")
-elif n < 0:
-    print("Negative")
-else:
-    print("Zero")
+#####if n > 0:
+    #print("Positive")
+#####elif n < 0:
+    #print("Negative")
+#####else:
+    #print("Zero")
+
+
+
+# String types
+name = "Harry"
+print(name[0]) # H
+
+# List types
+names = ["Harry", "Ron", "Hermione"]
+print(names[0]) # Harry
+print(names[1]) # Ron
+print(names[2]) # Hermione
