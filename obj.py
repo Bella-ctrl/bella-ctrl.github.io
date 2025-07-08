8799eb88e26754d7353911430149178245b45852
bella-ctrl <v.30612338.gb@gmail.com>
2025-07-08 13:15:13 -0400

    adding passengers

--- obj.py
+++ obj.py
@@ -1,15 +1,5 @@
 # Object Oriented Programming (OOP) in Python
 
-class Point():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-p = Point(2, 8)
-print(p.x)  # Output: 2
-print(p.y)  # Output: 8
-
-
 class Flight():
     def __init__(self, capacity):
         self.capacity = capacity
