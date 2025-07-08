e278e23a7f386824e18a9ae1a0b617dd30743149
bella-ctrl <v.30612338.gb@gmail.com>
2025-07-08 13:08:05 -0400

    object oriented programming

---
+++ obj.py
@@ -0,0 +1,10 @@
+# Object Oriented Programming (OOP) in Python
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+p = Point(2, 8)
+print(p.x)  # Output: 2
+print(p.y)  # Output: 8
