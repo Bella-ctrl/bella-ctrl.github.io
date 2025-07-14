d86b405785b26ff3418f6a7bbb948fa23b67f126
Bella-ctrl <v.30612338.gb@gmail.com>
2025-07-14 16:39:03 +0000

    Splitting name methods

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 print("hello, world")
 name = input("What is your name? ").strip().title()
 print("Hello, " + name)
-print("Hello,", name, "again!")
+
+# Split user's name into first and last name
+first, last = name.split(" ")
+
+print(f"Hello, {first}", "again!")
