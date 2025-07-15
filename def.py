4660a0a00ec3ae74f1fbad684f4023e4a142b3b3
Bella-ctrl <v.30612338.gb@gmail.com>
2025-07-15 15:46:53 +0000

    experimenting with functions

---
+++ def.py
@@ -0,0 +1,5 @@
+def hello(to):
+    print("Hello,", to)
+
+name = input("What is your name? ").strip().title()
+hello(name)
--- hello.py
+++ hello.py
@@ -11,3 +11,14 @@ x = int(input("Enter a number: "))
 y = int(input("Enter another number: "))
 
 print(f"The sum of {x} and {y} is {(x) + (y)}")
+
+m = int(input("Enter a number: "))
+if m % 2 == 0:
+    print(f"{m} is even")
+else:
+    print(f"{m} is odd")
+
+n = int(input("Enter a number: "))
+print(f"The numbered entered is {n:,}")
+print(f"The number entered is {n:.2f}")
+
