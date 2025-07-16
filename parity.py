bbda2df35371d59a4dd9e895936332e47a6517d0
Bella-ctrl <v.30612338.gb@gmail.com>
2025-07-16 19:10:03 +0000

    using % and functions

---
+++ parity.py
@@ -0,0 +1,16 @@
+# arithmetic operators = + - * / % ** //
+def main():
+    x = int(input("What's X? "))
+
+    if is_even(x):
+        print("X is even")
+    else:
+        print("X is odd")
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+    
+main()
