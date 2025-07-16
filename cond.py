b6f1f4e168821db76b64d239a6f19fc4e6f4e507
Bella-ctrl <v.30612338.gb@gmail.com>
2025-07-16 18:46:14 +0000

    and keyword

--- cond.py
+++ cond.py
@@ -2,17 +2,30 @@
 ### symbols:
 #### > >= < <= == !=
 
-x = int(input("What's X? "))
-y = int(input("What's Y? "))
+#x = int(input("What's X? "))
+#y = int(input("What's Y? "))
 
-if x < y:
-    print("X is less than Y")
-elif x > y:
-    print("X is greater than Y")
-else:
-    print("X is equal to Y")
+#if x < y:
+ #   print("X is less than Y")
+#elif x > y:
+  #  print("X is greater than Y")
+#else:
+ #   print("X is equal to Y")
 
-if x < y or x > y:
-    print("X is not equal to Y")
+###if x < y or x > y:
+   # print("X is not equal to Y")
+#else:
+   # print("X is equal to Y")
+
+
+score = int(input("Score: "))
+if score >= 90 and score <= 100:
+    print("A")
+elif score >= 80 and score < 90:
+    print("B")
+elif score >= 70 and score < 80:
+    print("C")
+elif score >= 60 and score < 70:
+    print("D")
 else:
-    print("X is equal to Y")
+    print("F")
