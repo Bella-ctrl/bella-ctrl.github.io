a4a241bf1071bf2285c0ed13d8fe68bccbd2bbc1
bella-ctrl <v.30612338.gb@gmail.com>
2025-07-21 10:11:24 -0400

    for i in range len n

--- pp/hogwarts.py
+++ pp/hogwarts.py
@@ -1,6 +1,10 @@
 # List of students
 students = ["Hermione", "Harry", "Ron"]
-for student in students:
-    print(student)
-    
-print(students[0])
+
+for i in range(len(students)):
+#for student in students:
+    print(i + 1, students[i])
+    #print(students[i])
+
+
+#print(students[0])
