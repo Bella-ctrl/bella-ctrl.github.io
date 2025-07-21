03bcfe9d3944b18965caccf42438af09bd9aa4ed
bella-ctrl <v.30612338.gb@gmail.com>
2025-07-21 10:19:37 -0400

    dicts and loop for

--- pp/hogwarts.py
+++ pp/hogwarts.py
@@ -1,10 +1,26 @@
-# List of students
-students = ["Hermione", "Harry", "Ron"]
+# Dictionary of students
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+}
 
-for i in range(len(students)):
+for student in students:
+    print(f"{student} is in {students[student]}")
+
+
+
+# List of students
+#students = ["Hermione", "Harry", "Ron"]
+
+#for i in range(len(students)):
 #for student in students:
-    print(i + 1, students[i])
+ #   print(i + 1, students[i])
     #print(students[i])
 
 
 #print(students[0])
+
+
+
