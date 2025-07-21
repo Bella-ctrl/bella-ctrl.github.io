d9e069ee5b94696f668bdac6a41e62fd46b132d0
bella-ctrl <v.30612338.gb@gmail.com>
2025-07-21 10:03:30 -0400

    functions, while and for

--- pp/cat.py
+++ pp/cat.py
@@ -2,10 +2,26 @@
     # For i in [0, 1, 2]
     # print("Meow")
 
-while True:
-    n = int(input("Enter a number (0 to exit): "))
-    if n > 0:
-        break
+#while True:
+#    n = int(input("Enter a number (0 to exit): "))
+ ##      break
 
-for i in range(n):
-    print("Meow")
+#for i in range(n):
+ #   print("Meow")
+
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("Enter a number (0 to exit): "))
+        if n > 0:
+            break
+    return n
+
+def meow(n):
+    for _ in range(n):
+        print("Meow")
+
+main()
