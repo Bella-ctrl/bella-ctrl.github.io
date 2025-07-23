49d8b6ef2d477fc0c1da436e63d14a852398cf40
bella-ctrl <v.30612338.gb@gmail.com>
2025-07-23 09:30:45 -0400

    functions main and other

--- pp/hi.py
+++ pp/hi.py
@@ -1,11 +1,17 @@
-while True:
-    try:
-        x = int(input("What's x? "))
-        # break could be here 
-    except ValueError:
-        print("Invalid input. Please enter a number.")
-    else:
-        # One way of solving it
-        break
+def main():
+    x = get_int()
+    print(f"x is {x}")
 
-print(f"x is {x}")
+def get_int():
+    while True:
+        try:
+            x = int(input("What's x? "))
+            # break could be here 
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+        else:
+            # One way of solving it
+            break
+    return x
+
+main()
