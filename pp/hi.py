ad90b095b6812b8f5bfabcbec414f7652c611f63
bella-ctrl <v.30612338.gb@gmail.com>
2025-07-23 09:12:17 -0400

    fixed NameError with an else break and while true loop

--- pp/hi.py
+++ pp/hi.py
@@ -1,6 +1,11 @@
-try:
-    x = int(input("What's x? "))
-except ValueError:
-    print("Invalid input. Please enter a number.")
-else:
-    print(f"x is {x}")
+while True:
+    try:
+        x = int(input("What's x? "))
+        # break could be here 
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+    else:
+        # One way of solving it
+        break
+
+print(f"x is {x}")
