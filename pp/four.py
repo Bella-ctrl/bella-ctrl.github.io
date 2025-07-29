ae7f6a8a0a1f3d8dd7ae74cdc910620de7b289d3
bella-ctrl <v.30612338.gb@gmail.com>
2025-07-29 11:22:32 -0400

    random.shuffle

--- pp/four.py
+++ pp/four.py
@@ -10,5 +10,12 @@
 
 
 import random
-number = random.randint(1, 10)
-print("The random number is:", number)
+#number = random.randint(1, 10)
+#print("The random number is:", number)
+
+# shuffle 
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+for card in cards:
+    print(card) 
+
