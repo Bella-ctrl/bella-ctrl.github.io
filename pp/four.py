d706f20adf47bf6c33fa280591b5e439b00c3219
bella-ctrl <v.30612338.gb@gmail.com>
2025-07-29 11:18:17 -0400

    used random for flipping a coin but with from

--- pp/four.py
+++ pp/four.py
@@ -1,4 +1,9 @@
-import random
+#import random """WITH THIS YOU IMPORT EVERYTHING
 
-coin = random.choice(["heads", "tails"])
+#coin = random.choice(["heads", "tails"])
+#print("The coin landed on:", coin)
+
+from random import choice  # Only import the choice function
+
+coin = choice(["heads", "tails"])
 print("The coin landed on:", coin)
