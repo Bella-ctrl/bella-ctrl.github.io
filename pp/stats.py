f5c79df7155ffae2a285eb3e871465f4f4fd60fd
bella-ctrl <v.30612338.gb@gmail.com>
2025-07-29 11:27:19 -0400

    statistics.mean

--- pp/stats.py
+++ pp/stats.py
@@ -1,4 +1,6 @@
 #docs.python.org/3/library/statistics.html
-import statistics
+##import statistics
+##print(statistics.mean([100, 90, 70]))
 
-print(statistics.mean([100, 90, 70]))
+
+#command line arguments = arg when running the script
