8a201ddcbecb23c088430eea38a6682d7ca6d403
bella-ctrl <v.30612338.gb@gmail.com>
2025-07-29 11:35:56 -0400

    sys indexError

--- pp/eh.py
+++ pp/eh.py
@@ -2,5 +2,11 @@
 # sys.argv is used to get the command line arguments
 
 import sys
-
-print("hello, my name is", sys.argv[1])
+#try:
+ 
+if len(sys.argv) < 2:
+    print("Usage: python eh.py <name>")
+elif len(sys.argv) > 2:
+    print("Too many arguments provided")
+else:
+    print("hello, my name is", sys.argv[1])
