a887295cac1c2c9a4d165b978a46e6c594f71f92
bella-ctrl <v.30612338.gb@gmail.com>
2025-07-30 08:27:01 -0400

    sys.exit and slicing in args

--- pp/eh.py
+++ pp/eh.py
@@ -5,8 +5,11 @@ import sys
 #try:
  
 if len(sys.argv) < 2:
-    print("Usage: python eh.py <name>")
-elif len(sys.argv) > 2:
-    print("Too many arguments provided")
-else:
-    print("hello, my name is", sys.argv[1])
+    sys.exit("Too few arguments provided")
+#elif len(sys.argv) > 2:
+  #  sys.exit("Too many arguments provided")
+#else:
+ #   print("hello, my name is", sys.argv[1])
+
+for arg in sys.argv[1:]:
+   print("hello, my name is", arg)
