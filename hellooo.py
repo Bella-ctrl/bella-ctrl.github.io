a4cb647b3307a7f79347a219129f7c0dff04e1dd
Bella-ctrl <v.30612338.gb@gmail.com>
2025-08-27 20:47:52 +0000

    hellooo.py

--- hellooo.py
+++ hellooo.py
@@ -1,1 +1,16 @@
 ## Just to have a commit todat
+## Just to have a commit todat
+class Hello():
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(name):
+        return f"Hello {self.name}, nice to meet you!"
+
+def main():
+    salute = Hello()
+    name = input("Introduce your name: ")
+    print(Hello(name))
+
+if __name__ == "__main__":
+    main()
